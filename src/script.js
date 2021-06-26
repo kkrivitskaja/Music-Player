9b69032c0d866cde88af457d267509c191185b4b
kkrivitskaja <pratugames@gmail.com>
2021-06-26 16:08:10 +0300

    feat:add  progress bar

--- src/script.js
+++ src/script.js
@@ -24,30 +24,30 @@ function loadSong(song) {
 
 function playSong() {
     musicContainer.classList.add('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
     audio.play();
 }
 
 function pauseSong() {
-     musicContainer.classList.remove('play');
-     playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
     playBtn.querySelector('i.fas').classList.add('fa-play');
     audio.pause();
 }
 
-function prevSong(){
-    songIndex--
-    
+function prevSong() {
+    songIndex--;
+
     if (songIndex < 0) {
         songIndex = songs.length - 1;
     }
-    loadSong(songs[songIndex])
+    loadSong(songs[songIndex]);
     playSong();
 }
 
-function nextSong(){
-    songIndex++
+function nextSong() {
+    songIndex++;
     if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
@@ -55,6 +55,18 @@ function nextSong(){
     playSong();
 }
 
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
 
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -68,3 +80,9 @@ playBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+progressContainer.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
